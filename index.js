e8dc83ad2dd814aaaecf332260399a5a3059d4c3
amogal <31324456+amogal@users.noreply.github.com>
2023-02-09 11:04:31 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3030;
+
+app.get('/', (req, res) => res.send('This is a test Node JS for Aws App Runner!'))
+
+app.listen(port, (err) => {
+    if (err) {
+      console.log('Error::', err);
+    }
+      console.log(`Aws App Runner NodeJs app listening on port ${port}`);
+  });
