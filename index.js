94a7c90c78428675638626ac5380339d11e8f1f0
amogal <31324456+amogal@users.noreply.github.com>
2023-02-10 00:16:05 -0600

    Update index.js
    updated text

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 const port = process.env.PORT || 3030;
 
-app.get('/', (req, res) => res.send('This is a test Node JS for Aws App Runner!'))
+app.get('/', (req, res) => res.send('This is a test Node JS app for Aws App Runner!'))
 
 app.listen(port, (err) => {
     if (err) {
